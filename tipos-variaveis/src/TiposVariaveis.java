d59849fef25c626381f1d815d7690df6da464e6a
barbaraceli <barbaraceli@id.uff.br>
2023-10-14 14:44:30 -0300

    atualização

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+       //estudem:
+       //tipos primitivos
+       //classe String que representa texto na aplicação
+
+       String meuNome = "Bárbara Celi"
+
+       
+       
+       
         System.out.println("Hello, World!");
     }
 }
